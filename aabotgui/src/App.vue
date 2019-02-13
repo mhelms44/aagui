52d17ebd9c9124f60d989bfa9e6794688dfc8930
Michael Helms <michael.helms@nutanix.com>
2019-02-13 14:17:13 -0500

    Side menu created

--- aabotgui/src/App.vue
+++ aabotgui/src/App.vue
@@ -1,37 +1,23 @@
 <template>
-  <v-app dark>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+  <v-app :dark ='blah'>
+    <side-menu/>
     <v-content>
-      <HelloWorld/>
+      <hello-thar/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MyMenu from './components/SideMenu'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SideMenu
   },
   data () {
     return {
-      //
+      blah:true
     }
   }
 }
